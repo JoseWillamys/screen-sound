32488955958c9e99aaae5e02444cb9b070ab4e6f
José Willamys <willamysoad@gmail.com>
2026-05-25 19:18:43 -0300

    feat: adiciona método ExibirFichaTecnica para a classe Musica

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,10 +1,16 @@
 ﻿Musica musica1 = new Musica();
 musica1.nome = "Roxane";
 musica1.artista = "The Police";
+musica1.duração = 273;
+musica1.disponivel = true;
+
+musica1.ExibirFichaTecnica();
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
+musica2.duração = 367;
+musica2.disponivel = false;
 
-Console.WriteLine($"Nome da Música: {musica1.nome}");
-Console.WriteLine($"Artista: {musica1.artista}");
+Console.WriteLine("");
+musica2.ExibirFichaTecnica();
