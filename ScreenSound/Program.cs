eeda3d971a3837e29d675dda795fa0dac1920503
José Willamys <willamysoad@gmail.com>
2026-06-02 22:55:58 -0300

    feat: adiciona a classe Genero

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,35 +1,44 @@
-﻿Album albumToxicity = new Album();
-albumToxicity.Nome = "Toxicity";
+﻿Album albumToxicity = new() { Nome = "Toxicity" };
 
-Musica musica1 = new Musica();
-musica1.Nome = "Prinson Song";
-musica1.Artista = "System of a Down";
-musica1.Duracao = 273;
-musica1.Disponivel = true;
+Musica musica1 = new()
+{
+    Nome = "Prinson Song",
+    Artista = "System of a Down",
+    Duracao = 273,
+    Disponivel = true
+};
 
-Musica musica2 = new Musica();
-musica2.Nome = "Chop Suey!";
-musica2.Artista = "System of a Down";
-musica2.Duracao = 210;
-musica2.Disponivel = false;
+Musica musica2 = new()
+{
+    Nome = "Chop Suey!",
+    Artista = "System of a Down",
+    Duracao = 210,
+    Disponivel = false
+};
 
-Musica musica3 = new Musica();
-musica3.Nome = "Bounce";
-musica3.Artista = "System of a Down";
-musica3.Duracao = 114;
-musica3.Disponivel = false;
+Musica musica3 = new()
+{
+    Nome = "Bounce",
+    Artista = "System of a Down",
+    Duracao = 114,
+    Disponivel = false
+};
 
-Musica musica4 = new Musica();
-musica4.Nome = "Toxicity";
-musica4.Artista = "System of a Down";
-musica4.Duracao = 218;
-musica4.Disponivel = false;
+Musica musica4 = new()
+{
+    Nome = "Toxicity",
+    Artista = "System of a Down",
+    Duracao = 218,
+    Disponivel = false
+};
 
-Musica musica5 = new Musica();
-musica5.Nome = "Aerials";
-musica5.Artista = "System of a Down";
-musica5.Duracao = 235;
-musica5.Disponivel = false;
+Musica musica5 = new()
+{
+    Nome = "Aerials",
+    Artista = "System of a Down",
+    Duracao = 235,
+    Disponivel = false
+};
 
 albumToxicity.AdicionarMusica(musica1);
 albumToxicity.AdicionarMusica(musica2);
